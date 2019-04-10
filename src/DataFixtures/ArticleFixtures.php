28b02ec30b8a6bac6dab40a70f3ebc85f4f15652
Wanderer <cj.mail.acc@gmail.com>
2019-04-10 10:58:36 +0300

    finish

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -4,9 +4,11 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\Tag;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixture
+class ArticleFixtures extends BaseFixture implements DependentFixtureInterface
 {
     private static $articleTitles = [
         'Why Asteroids Taste Like Bacon',
@@ -24,6 +26,13 @@ class ArticleFixtures extends BaseFixture
         'Mike Ferengi',
         'Amy Oort',
     ];
+
+    public function getDependencies()
+    {
+        return [
+            TagFixture::class,
+        ];
+    }
 
     public function loadData(ObjectManager $manager)
     {
@@ -58,6 +67,11 @@ EOF
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages))
             ;
+
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
+            foreach ($tags as $tag) {
+                $article->addTag($tag);
+            }
         });
 
         $manager->flush();
